ebb94e2fb28df62ccb4899c39922414aa3f8482c
didedoshka <didedoshka@gmail.com>
2023-07-30 22:31:13 +0300

    finish init and add setting from_route

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class State:
     def __init__(self):
         self.text = ''
@@ -13,26 +14,38 @@ class State:
             return True
         return False
 
+
 class YandexMapsInterface:
+    ENTER = '\uE006'
+
+    @staticmethod
+    def find_element(where, params):
+        return WebDriverWait(where, timeout=3).until(EC.presence_of_element_located(params))
+
+    @staticmethod
+    def find_elements(where, params):
+        return WebDriverWait(where, timeout=3).until(EC.presence_of_all_elements_located(params))
+
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, timeout=3)
         self.driver.get('https://www.yandex.ru/maps')
 
-        elem = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'route-control'))) # enter routes mode
-        elem.click()
-        # self.wait.find_element(By.CLASS_NAME, 'route-control').click() # enter routes mode
+        self.find_element(self.driver, (By.CLASS_NAME, 'route-control')).click()  # enter routes mode
 
-        # route_panel = self.driver.find_element(By.CLASS_NAME, 'route-panel-form-view__content')
-        #
-        # route_panel.find_element(By.CLASS_NAME, '_mode_masstransit').click() # select public transport
-        #
-        # inputs_in_route_panel = route_panel.find_elements(By.TAG_NAME, 'input')
-        # self.route_from = inputs_in_route_panel[0]
-        # self.route_to = inputs_in_route_panel[1]
-        #
+        route_panel = self.find_element(self.driver, (By.CLASS_NAME, 'route-panel-form-view__content'))
+        self.find_element(route_panel, (By.CLASS_NAME, '_mode_masstransit')).click()  # select public transport
+
+        inputs_in_route_panel = self.find_elements(route_panel, (By.TAG_NAME, 'input'))
+        self.route_from = inputs_in_route_panel[0]
+        self.route_to = inputs_in_route_panel[1]
+
+    def set_route_from(self, street_name_from):
+        self.route_from.send_keys(street_name_from + self.ENTER)
 
 
 ymi = YandexMapsInterface()
+
+ymi.set_route_from('Покровский бульвар, 11с10')
 
 input()
