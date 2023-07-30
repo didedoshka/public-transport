ec890fc91938da06a6c98c817f4147e2ce526445
didedoshka <didedoshka@gmail.com>
2023-07-30 22:59:43 +0300

    add duration change detection

--- test.py
+++ test.py
@@ -17,14 +17,15 @@ class State:
 
 class YandexMapsInterface:
     ENTER = '\uE006'
+    TIMEOUT = 10
 
-    @staticmethod
-    def find_element(where, params):
-        return WebDriverWait(where, timeout=3).until(EC.presence_of_element_located(params))
+    @classmethod
+    def find_element(cls, where, params):
+        return WebDriverWait(where, timeout=cls.TIMEOUT).until(EC.presence_of_element_located(params))
 
-    @staticmethod
-    def find_elements(where, params):
-        return WebDriverWait(where, timeout=3).until(EC.presence_of_all_elements_located(params))
+    @classmethod
+    def find_elements(cls, where, params):
+        return WebDriverWait(where, timeout=cls.TIMEOUT).until(EC.presence_of_all_elements_located(params))
 
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -33,19 +34,29 @@ class YandexMapsInterface:
 
         self.find_element(self.driver, (By.CLASS_NAME, 'route-control')).click()  # enter routes mode
 
-        route_panel = self.find_element(self.driver, (By.CLASS_NAME, 'route-panel-form-view__content'))
-        self.find_element(route_panel, (By.CLASS_NAME, '_mode_masstransit')).click()  # select public transport
+        self.route_panel = self.find_element(self.driver, (By.CLASS_NAME, 'route-panel-form-view__content'))
+        self.find_element(self.route_panel, (By.CLASS_NAME, '_mode_masstransit')).click()  # select public transport
 
-        inputs_in_route_panel = self.find_elements(route_panel, (By.TAG_NAME, 'input'))
+        inputs_in_route_panel = self.find_elements(self.route_panel, (By.TAG_NAME, 'input'))
         self.route_from = inputs_in_route_panel[0]
         self.route_to = inputs_in_route_panel[1]
+
+        self.duration_state = State()
 
     def set_route_from(self, street_name_from):
         self.route_from.send_keys(street_name_from + self.ENTER)
 
+    def set_route_to(self, street_name_to):
+        self.route_to.send_keys(street_name_to + self.ENTER)
+
+    def get_duration(self):
+        WebDriverWait(self.route_panel, timeout=self.TIMEOUT).until(lambda where: self.duration_state.did_change(
+            where.find_element(By.CLASS_NAME, 'route-snippet-view').get_attribute('innerHTML')))
+        print(self.duration_state.text)
 
 ymi = YandexMapsInterface()
 
 ymi.set_route_from('Покровский бульвар, 11с10')
-
+ymi.set_route_to('Кременчугская, 11')
+ymi.get_duration()
 input()
